a7de15e0c4f904aaf598c2a1d8fc2cb28777425e
PatriciaAlEs <168550797+PatriciaAlEs@users.noreply.github.com>
2024-10-23 18:36:01 +0000

    jwt OK segunda vez

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -16,8 +16,8 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+        <Link to="/signup">
+          <span className="navbar-brand mb-0 h1">Registro</span>
         </Link>
         <div className="ml-auto">
           <button onClick={handleLogout}>Logout</button>
